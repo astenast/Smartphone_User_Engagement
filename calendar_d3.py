b9ebb6b5ac5bc579dd7f31501e8c306709c3b576
Asterios <asteriosnastas@gmail.com>
2021-11-30 20:36:37 +0100

    calendar fxs, to see more calendars set target []

--- calendar_d3.py
+++ calendar_d3.py
@@ -4,7 +4,7 @@ from streamlit_observable import observable
 
 @st.cache(allow_output_mutation=True)
 def get_calendar_data():
-    return pd.read_csv("https://raw.githubusercontent.com/astenast/Smartphone_User_Engagement/main/data/calendar_user_389_final.csv?token=AMWUYUNDOQME4QGHBVX3MVTBUKLDI")
+    return pd.read_csv("https://raw.githubusercontent.com/astenast/Smartphone_User_Engagement/main/data/calendar_user_389_final_w_dailyduration.csv?token=AMWUYUOHKPTGOAEFAV75FF3BV6Y3G")
 
 
 def app():
@@ -33,5 +33,11 @@ def app():
 
     observable("Calendar", 
         notebook="@astenast/digital_media_project",
-        targets=["chart"], #"chart", "key" "chart2", "chart3", "chart4", "chart5", "other"
+        targets=["chart_w_daily2"], #"chart", "key" "chart2", "chart3", "chart4", "chart5", "other"
+        
+    # observable("Calendar", 
+    #     notebook="@astenast/digital_media_project",
+    #     targets=[], #"chart", "key" "chart2", "chart3", "chart4", "chart5", "other"
+
+    
     )
